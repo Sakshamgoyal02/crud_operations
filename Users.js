04493481969883105eeea3ee7fd545e753467159
Sakshamgoyal02 <sakshamgoyal142@gmail.com>
2025-09-25 16:52:59 +0530

    fetch member

---
+++ Users.js
@@ -0,0 +1,21 @@
+module.exports = [
+    
+    { "id": 1,
+      "name": "John Doe",
+      "email":"John123@gmail.com"
+    },
+
+    { "id": 2,
+      "name": "Jane Doe",
+      "email":"Jane123@gmail.com"
+    },
+
+    { "id": 3,
+      "name": "Simon",
+      "email":"Simon123@gmail.com"
+    },
+
+]
+
+
+
